a5580a272872c44ed660ebd62f7bef7b50f93445
XiaominQu <XiaominQu121@163.com>
2018-11-20 18:29:49 +0800

    完成了首页

--- src/main.js
+++ src/main.js
@@ -13,6 +13,20 @@ import "./fonts/mui.ttf"
 import "./css/icons-extra.css"
 import "./fonts/mui-icons-extra.ttf"
 
+
+// 引入轮播图组件并注册
+import { Swipe, SwipeItem } from 'mint-ui';
+
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+
+// 发送请求获取数据vue里面用到vue-resource
+//导入并注册
+import vueResource from "vue-resource"
+Vue.use(vueResource)
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
